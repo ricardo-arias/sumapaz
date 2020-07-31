48d6af23d4f60612bef281818318670c5ecfd626
ricardo-arias <69052076+ricardo-arias@users.noreply.github.com>
2020-07-31 13:52:09 -0500

    Update storymap.js

--- scripts/storymap.js
+++ scripts/storymap.js
@@ -88,10 +88,10 @@ $(window).on('load', function() {
     // Add logo
     if (getSetting('_mapLogo')) {
       $('#logo').append('<img src="' + getSetting('_mapLogo') + '" />');
-      $('#top').css('height', '60px');
+      $('#top').css('height', '100px');
     } else {
       $('#logo').css('display', 'none');
-      $('#header').css('padding-top', '25px');
+      $('#header').css('padding-top', '100px');
     }
 
     // Load tiles
