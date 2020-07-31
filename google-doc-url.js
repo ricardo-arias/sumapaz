5f5599d3e99e7782e7cc85276abf6c34dff92bbf
ricardo-arias <69052076+ricardo-arias@users.noreply.github.com>
2020-07-31 13:06:54 -0500

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,1 +1,1 @@
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1AO6XHL_0JafWZF4KEejkdDNqfuZWUk3SlNlQ6MjlRFM/';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1uhvL6P4dy_rLiy_rNYiAOmNQBWoV-HBXBplAw-1JXfQ/edit#gid=0';
